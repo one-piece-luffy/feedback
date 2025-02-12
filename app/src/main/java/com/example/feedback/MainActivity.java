637966c14b36050f65638e47d0c678aab4aa28a1
one piece <646796992@qq.com>
2025-02-12 18:59:10 +0800

    1

--- app/src/main/java/com/example/feedback/MainActivity.java
+++ app/src/main/java/com/example/feedback/MainActivity.java
@@ -8,15 +8,10 @@ import android.util.Log;
 import android.view.View;
 
 import com.baofu.feedback.RateDialog;
-import com.baofu.netlibrary.BPConfig;
-import com.baofu.netlibrary.BPListener;
-import com.baofu.netlibrary.BPRequest;
-import com.baofu.netlibrary.listener.RequestListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,22 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Map<String, String> header = new HashMap<>();
         header.put("User-Agent", "UA");
-        BPConfig config=new BPConfig.Builder().context(this).strategyType(BPRequest.STRATEGY_TYPE.OKHTTP).addHeader(header)
-//                .banProxy(true)
-                .setRequestListener(new RequestListener() {
-                    @Override
-                    public String responseListener(Headers headers, int status, String url, String response) {
-                        return response;
-                    }
-
-                    @Override
-                    public String exceptionListener(String url, String error, Exception e, int code) {
-                        return null;
-                    }
-
-                })
-                .build();
-        BPRequest.getInstance().init(config);
         RateDialog rateDialog = new RateDialog(MainActivity.this,true, new RateDialog.RateDialogListener() {
             @Override
             public void onRate() {
